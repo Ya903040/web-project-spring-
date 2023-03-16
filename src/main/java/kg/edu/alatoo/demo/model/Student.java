52c8fdd74533a0d4e7bec21a4d0774a3fc638913
Ya903040 <asirov560@gmail.com>
2023-03-16 15:09:14 +0600

    first commit

---
+++ src/main/java/kg/edu/alatoo/demo/model/Student.java
@@ -0,0 +1,40 @@
+package kg.edu.alatoo.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private double gpa;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student (name, gpa) VALUES ('Aibek', 3.5);
+INSERT INTO student (name, gpa) VALUES ('Suleiman', 3.8);
+INSERT INTO student (name, gpa) VALUES ('a', 3.8);
+INSERT INTO student (name, gpa) VALUES ('d', 3.8);
